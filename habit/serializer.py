00f19e2e52a8136f38206307d1430306565ef7c3
Syed Afnan <afnanhfs016@gmail.com>
2026-06-27 08:40:48 +0500

    add nested serializer

--- habit/serializer.py
+++ habit/serializer.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Habit, HabitLog
 
-class HabitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habit
-        fields = ['id', 'name', 'frequency', 'created_at']
-
 class HabitLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = HabitLog
         fields = ['id', 'habit', 'date', 'completed']
         
+    
+class HabitSerializer(serializers.ModelSerializer):
+    
+    habitlogs = HabitLogSerializer(many = True, read_only= True)
+
+    class Meta:
+        model = Habit
+        fields = ['id', 'name', 'frequency', 'created_at', 'habitlogs']
